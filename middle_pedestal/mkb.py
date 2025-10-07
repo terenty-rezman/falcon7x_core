cd8198daa98ec2199c8e6eb263ef5552d20f48b0
terenty.rezman <terenty.rezman@mail.ru>
2025-10-07 17:25:36 +0300

    mixa job

--- middle_pedestal/mkb.py
+++ middle_pedestal/mkb.py
@@ -29,3 +29,915 @@ class mkb_c_rh(NLocalStateButton):
     @classmethod
     async def click(cls):
         await super().click()
+
+@add_to_panel
+class mkb_d_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_e_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_f_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_g_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_h_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_i_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_j_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_k_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_l_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_m_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_n_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_o_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_p_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_q_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_r_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_s_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_t_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_u_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_v_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_w_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_x_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_y_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_z_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_1_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_2_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_3_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_4_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_5_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_6_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_7_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_8_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_9_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_0_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+@add_to_panel
+class mkb_BKUP_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+
+@add_to_panel
+class mkb_HF_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+
+@add_to_panel
+class mkb_SAT_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_ATC_DTLC_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_XPDR_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_BKUP_NAV_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_TERR_INHIB_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_GS_INHIB_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_EMPTY1_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_TRFC_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_CRS_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_SHOW_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_DIRTO_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_EMPTY2_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_ID_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_HSI_RANGE_IO_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_OFF_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_STBY_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_AUTO_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_GAIN_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_TILT_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_OVRD_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_I0_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_WX_RADAR_PUSH_SECT_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_CL_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_LEFT_arrow_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_SPACE_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_RIGHT_arrow_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_CLR_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_SHIFT_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_ENTER_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_SWAP_rh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+
+@add_to_panel
+class mkb_a_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+
+@add_to_panel
+class mkb_b_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+
+@add_to_panel
+class mkb_c_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_d_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_e_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_f_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_g_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_h_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_i_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_j_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_k_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_l_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_m_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_n_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_o_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_p_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_q_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_r_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_s_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_t_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_u_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_v_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_w_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_x_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_y_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_z_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+@add_to_panel
+class mkb_1_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_2_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_3_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_4_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_5_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_6_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_7_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_8_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_9_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
+
+@add_to_panel
+class mkb_0_lh(NLocalStateButton):
+    states = [0, 1]
+    state = 0
+
+    @classmethod
+    async def click(cls):
+        await super().click()
