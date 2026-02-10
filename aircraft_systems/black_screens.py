35464768ddde8ac7653c691e82236d936b1a875f
terenty.rezman <terenty.rezman@mail.ru>
2026-02-10 17:33:26 +0300

    uso trim + black screens

--- aircraft_systems/black_screens.py
+++ aircraft_systems/black_screens.py
@@ -16,113 +16,159 @@ import overhead_panel.dc_supply as dc
 
 
 class LeftBlackScreen(System):
+    next_wake_sleep_delay = 2
+
     LEFT = xp.Params["sim/custom/7x/z_left_black_screen"]
 
     logic_task = None
     is_killing = False
 
+    prev_state = None
+
     @classmethod
     def start_condition(cls):
         return True
 
     @classmethod
     async def system_logic_task(cls):
+        new_state = None
+
         if dc.bat1.get_state() == 0:  
-            await xp.set_param(cls.LEFT, 1)
-        
+            new_state = 1
         else:
             if dc.rh_init.get_state() == 1: 
-                await xp.set_param(cls.LEFT, 2)
+                new_state = 2
             else:
-                await sim.sleep(3)
-                await xp.set_param(cls.LEFT, 0)
+                new_state = 0
+        
+        if new_state != cls.prev_state:
+            cls.prev_state = new_state
+            await xp.set_param(cls.LEFT, new_state)
 
 
 class MiddleUpBlackScreen(System):
+    next_wake_sleep_delay = 2
+
     MIDDLE_UP = xp.Params["sim/custom/7x/z_middle_up_black_screen"]
 
     logic_task = None
     is_killing = False
 
+    prev_state = None
+
     @classmethod
     def start_condition(cls):
         return True
 
     @classmethod
     async def system_logic_task(cls):
+        new_state = None
+
         if dc.bat1.get_state() == 0:  
-            await xp.set_param(cls.MIDDLE_UP, 1)
+            new_state = 1
         else:
             if dc.rh_init.get_state() == 1:
-                await xp.set_param(cls.MIDDLE_UP, 2)
+                new_state = 2
             elif dc.rh_init.get_state() == 0 and dc.lh_master.get_state() == 0 and dc.rh_master.get_state() == 1: 
-                await xp.set_param(cls.MIDDLE_UP, 2)
+                new_state = 2
             else:
-                await xp.set_param(cls.MIDDLE_UP, 0)
+                new_state = 0
+
+        if new_state != cls.prev_state:
+            cls.prev_state = new_state
+            await xp.set_param(cls.MIDDLE_UP, new_state)
 
 
 class RightBlackScreen(System):
+    next_wake_sleep_delay = 2
+
     RIGHT = xp.Params["sim/custom/7x/z_right_black_screen"]
 
     logic_task = None
     is_killing = False
 
+    prev_state = None
+
     @classmethod
     def start_condition(cls):
         return True
 
     @classmethod
     async def system_logic_task(cls):
+        new_state = None
+
         if dc.bat2.get_state() == 0:  
-            await xp.set_param(cls.RIGHT, 1)
+            new_state = 1
         else:
             if dc.lh_master.get_state() == 1 and dc.rh_master.get_state() == 1: 
-                await xp.set_param(cls.RIGHT, 2)
+                new_state = 2
             else:
-                await xp.set_param(cls.RIGHT, 0)
+                new_state = 0
+
+        if new_state != cls.prev_state:
+            cls.prev_state = new_state
+            await xp.set_param(cls.RIGHT, new_state)
             
 
 class MiddleDownBlackScreen(System):
+    next_wake_sleep_delay = 2
+
     MIDDLE_DOWN = xp.Params["sim/custom/7x/z_middle_down_black_screen"]
 
     logic_task = None
     is_killing = False
 
+    prev_state = None
+
     @classmethod
     def start_condition(cls):
         return True
 
     @classmethod
     async def system_logic_task(cls):
+        new_state = None
+
         if dc.bat1.get_state() == 0:  
-            await xp.set_param(cls.MIDDLE_DOWN, 1)
+            new_state = 1
         else:
             if dc.rh_init.get_state() == 0 and dc.lh_master.get_state() == 0 and dc.rh_master.get_state() == 0: 
-                await xp.set_param(cls.MIDDLE_DOWN, 0)
+                new_state = 0
             elif dc.rh_isol.get_state() == 0:
-                await xp.set_param(cls.MIDDLE_DOWN, 2)
+                new_state = 2
             else:
-                await xp.set_param(cls.MIDDLE_DOWN, 1)
+                new_state = 1
+
+        if new_state != cls.prev_state:
+            cls.prev_state = new_state
+            await xp.set_param(cls.MIDDLE_DOWN, new_state)
 
 
 class MiniBlackScreen(System):
+    next_wake_sleep_delay = 2
+
     MINI = xp.Params["sim/custom/7x/z_mini_black_screen"]
 
     logic_task = None
     is_killing = False
 
+    prev_state = None
+
     @classmethod
     def start_condition(cls):
         return True
 
     @classmethod
     async def system_logic_task(cls):
+        new_state = None
+
         if dc.bat1.get_state() == 0:  
-            await xp.set_param(cls.MINI, 1)
+            new_state = 1
         else:
             if dc.lh_init.get_state() == 0:
-                await sim.sleep(2)
-                await xp.set_param(cls.MINI, 0)
+                new_state = 0 
             else:
-                await xp.set_param(cls.MINI, 1)
+                new_state = 1
 
+        if new_state != cls.prev_state:
+            cls.prev_state = new_state
+            await xp.set_param(cls.MINI, new_state)
