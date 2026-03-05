10bddf0da86feed744bcdc67bfd3fd0036bfcd7c
terenty.rezman <terenty.rezman@mail.ru>
2026-03-05 15:06:05 +0300

    fix

--- front_panel/autopilot.py
+++ front_panel/autopilot.py
@@ -26,6 +26,14 @@ class vhf_control_lh(FloatStepper):
 
     val_type = int
 
+    @classmethod
+    async def inc(cls):
+        await super().dec()
+
+    @classmethod
+    async def dec(cls):
+        await super().inc()
+
 
 @add_to_panel
 class vhf_push_lh(PushButton):
